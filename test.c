1e0b918a9d95a94c4a3a4371385bb489ad93bd59
Yuki Esumi <cmt19007@ict.nitech.ac.jp>
2023-08-09 17:00:11 +0900

    test

--- test.c
+++ test.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
-#include "pig2_system.h"
+
+// サイコロの目が1かどうかを判断する
+int judge(int me)
+{
+    int r;
+
+    // サイコロの目が1の時0を返す
+    if (me == 1)
+    {
+        r = 0;
+    }
+    // サイコロの目が1以外のとき1を返す
+    else
+        r = 1;
+
+    return (r);
+}
 
 int main(void){
     assert(judge(1) == 0);
